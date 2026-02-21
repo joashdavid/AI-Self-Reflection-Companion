23baf387057411abfb4e6b9ffffff8d95b8a2412
joashdavid <joashdavid7@gmail.com>
2026-02-21 23:45:06 +0530

    Made Modification for Versel Deployment 1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -64,16 +64,32 @@
 // }
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL!;
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
+function createPrismaClient() {
+  const pool = new Pool({
+    connectionString,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
+
+  const adapter = new PrismaPg(pool);
+
+  return new PrismaClient({
+    adapter,
+  });
+}
+
+export const prisma =
+  globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production")
   globalForPrisma.prisma = prisma;
