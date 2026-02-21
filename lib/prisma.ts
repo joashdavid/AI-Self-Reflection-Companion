0e21f543a79df949c0f440b323902aab7d4034c6
joashdavid <joashdavid7@gmail.com>
2026-02-22 00:16:39 +0530

    removed prisma adapter and installed prisma6

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -64,32 +64,14 @@
 // }
 
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
-
-const connectionString = process.env.DATABASE_URL!;
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-function createPrismaClient() {
-  const pool = new Pool({
-    connectionString,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-
-  const adapter = new PrismaPg(pool);
-
-  return new PrismaClient({
-    adapter,
-  });
-}
-
 export const prisma =
-  globalForPrisma.prisma ?? createPrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production")
   globalForPrisma.prisma = prisma;
